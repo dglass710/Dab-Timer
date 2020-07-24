ef972443e2835b20b91e8be73acf12ca7dcead22
dglass710 <66642674+dglass710@users.noreply.github.com>
2020-07-24 00:53:16 -0500

    Update DabTimer.py

--- DabTimer.py
+++ DabTimer.py
@@ -1,22 +1,6 @@
 import time
 class dab(object):
-    def fPrint(self, n):
-        if n > 59 and n % 60 == 0:
-            print('{} min'.format(n // 60))
-        elif n > 59:
-            print('{} min {:.{}f} sec'.format(n // 60, n % 60, self.prec))
-        else:
-            print('{:.{}f} sec'.format(n, self.prec))
-    def timer(self, n):
-        self.fPrint(n)
-        iTime = time.time()
-        p = 0
-        while p < n:
-            if time.time() - iTime > p + self.inc:
-                p += self.inc
-                t = n - p
-                self.fPrint(t)
-    def __init__(self, cortesy_time, heat_up_time, dab_time, increment, precision):
+    def __init__(self, cortesy_time, heat_up_time, dab_time, increment = 1, precision = 0):
         self.inc = increment
         self.prec = precision
         """heat_up_time is how many seconds it will count down for dab_time is how much time it will count up for"""
@@ -27,4 +11,21 @@ class dab(object):
         print('Cool Down')
         self.timer(dab_time)
         print('DAB TIME!!! CHAIRS!!!')
+    def timer(self, n):
+        self.fPrint(n)
+        iTime = time.time()
+        p = 0
+        while p < n:
+            if time.time() - iTime > p + self.inc:
+                p += self.inc
+                t = n - p
+                self.fPrint(t)
+    def fPrint(self, n):
+        if n > 59 and n % 60 == 0:
+            print('{} min'.format(n // 60))
+        elif n > 59:
+            print('{} min {:.{}f} sec'.format(n // 60, n % 60, self.prec))
+        else:
+            print('{:.{}f} sec'.format(n, self.prec))
 dab(10, 60, 90, 1, 0)
+
